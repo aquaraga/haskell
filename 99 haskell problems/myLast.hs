0159c55c439d42220ffc5287a638b170613d3203
Kumar <iyer.kumar83@gmail.com>
2013-05-17 17:24:18 +0530

    Starting to solve 99 haskell problems - Problem 1

---
+++ 99 haskell problems/myLast.hs
@@ -0,0 +1,11 @@
+myLast :: [a] -> a
+myLast [] = error "List is empty"
+myLast ([x]) =  x
+myLast (x:xs) = myLast xs
+
+
+myLast' :: [a] -> a
+myLast' x = case x of [] -> error "List is empty"
+                      [s] -> s
+                      (x:xs) -> myLast' xs
+					  
